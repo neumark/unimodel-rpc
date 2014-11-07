3a5a83dc1b057c9916e2f35e157fb020fda45715
Peter Neumark <peter.neumark@prezi.com>
2014-11-07 09:22:21 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='unimodelrpc',
+    description="Swagger support for unimodel-typed interfaces.",
+    version="0.1",
+    url='https://github.com/neumark/unimodel-rpc/',
+    author='Peter Neumark',
+    author_email='neumark.peter@gmail.com',
+    license='Apache License 2.0',
+    packages=find_packages()
+)
